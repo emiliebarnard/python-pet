9418d8f1933c6eb3d3a37b851eb76a92335f3c01
Emilie Menard Barnard <emiliebarnard@gmail.com>
2018-05-10 19:24:18 -0700

    added structure for PythonPet subclass

--- src/main.py
+++ src/main.py
@@ -1,10 +1,26 @@
-def printPet():
-	
-	# snake ascii:
-	             
-	
-	
-	print("/\/(8)-<")
-	
+class Pet():
+	def __init__(self):
+		self.petName = "Jemima"
+		self.petType = "Egg"
+	def printPet(self):
+		print()
+		print("?")
+		print()
 
-printPet()
+
+class Python(Pet):
+	def __init__(self):
+		self.petType = "Python"
+	def printPet(self):
+			print()
+			print("/\/(8)-<")
+			print()	
+
+x = Pet()
+x.printPet()
+print(x.petType)
+
+y = Python()
+y.printPet()
+print(y.petType)
+print(y.petName)

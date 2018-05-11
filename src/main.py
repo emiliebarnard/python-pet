243cb2dcba9eddac6e34d86ac4e86f5d0e212067
Emilie Menard Barnard <emiliebarnard@gmail.com>
2018-05-10 19:52:28 -0700

    added base code for levels and xp
    still need to test though

--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
+import math
+
 class Pet():
 	def __init__(self):
-		self.petName = "Jemima"
-		self.petType = "Egg"
+		self.name = "Jemima"
+		self.type = "Egg"
+		self.xp = 0
+		self.level = 0
 	def printPet(self):
 		print()
 		print("(?)")
 		print()
+	def xpIncrease(self, changeInXp):
+		self.xp += changeInXp
+		oldLevel = self.level
+		self.level = math.floor(self.xp/1000) # this factor may need to change
+		if self.level > oldLevel:
+			print("Level up!")
 
 
 class Python(Pet):
 	def __init__(self):
 		Pet.__init__(self)
-		self.petType = "Python"
+		self.type = "Python"
 	def printPet(self):
 			print()
 			print("/\/(8)-<")
